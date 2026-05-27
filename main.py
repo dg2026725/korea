8dbfe57f95e67ed3cf8f5a5e18d56f2e3493510f
dg2026725 <dg2026725@danggok.hs.kr>
2026-05-27 14:12:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-for i in range(25) :
-    for j in range(24) : 
-        box(pos = vec(i,j,0), color = vec(188/255, 255/255, 253/255), shininess = 0)
-for q in range(50) :
-    for w in range(15) :
-        box(pos = vec(q,w,0), color = vec(6/255, 64/255, 43/255), shininess = 0)
-for e in range(30) :
-    box(pos = vec(40,e,0), color = vec(6/255, 64/255, 43/255), shininess = 0)
-    box(pos = vec(15,e,0), color = vec(6/255, 64/255, 43/255), shininess = 0)
+box(pos = vec(0,0,0), size = vec(100,100,0), texture = 'https://i.imgur.com/u3haxbY.jpeg')  - 배경
