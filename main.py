88cf7e7662414eee98e7aa33be62ad3813e94912
dg2026725 <dg2026725@danggok.hs.kr>
2026-05-27 15:00:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ box(pos = vec(50,0,-25), size = vec(100,100,0), texture = 'https://i.imgur.com/u
 for q in range(50) :
     box(pos = vec(50,-50,q), size = vec(100,5,50), color = vec(214/255,217/255,216/255), shininess = 0)
     box(pos = vec(50,-45,q), size = vec(80,5,40), color = vec(214/255,217/255,216/255), shininess = 0)
+    box(pos = vec(50,-43,q), size = vec(75,3,35), color = vec(214/255,217/255,216/255), shininess = 0)
