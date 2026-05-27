5497e38008fcc7877d2b20d5ed220e989f794858
dg2026725 <dg2026725@danggok.hs.kr>
2026-05-27 14:55:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-box(pos = vec(0,0,0), size = vec(100,100,0), texture = 'https://i.imgur.com/u3haxbY.jpeg')  - 배경
+box(pos = vec(50,0,-25), size = vec(100,100,0), texture = 'https://i.imgur.com/u3haxbY.jpeg')
+for q in range(50) :
+    box(pos = vec(50,-50,q), size = vec(100,5,50), color = vec(214/255,217/255,216/255), shininess = 0)
