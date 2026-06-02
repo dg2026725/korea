b84a734a3984458a628f8fc00f7c9c17ee7595cc
dg2026725 <dg2026725@danggok.hs.kr>
2026-06-02 09:09:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,4 @@ for q in range(50) :
     box(pos = vec(50,-40,q), size = vec(55,10,5), color = vec(60/255,179/255,113/255), shininess = 0)
     box(pos = vec(50,-23,q), size = vec(55,25,5), color = vec(255/255,250/255,240/255), shininess = 0)
     box(pos = vec(50,-35,53), size = vec(10,20,1), color = vec(254/255,245/255,230/255), shininess = 0)
+    cylinder(pos = vec(50,30,50), size = vec(50,50,10), color = vec(0/255,0/255,0/255), shininess = 0)
