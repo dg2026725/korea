92d9935360d8e358f9356186097effdf602ee178
dg2026725 <dg2026725@danggok.hs.kr>
2026-06-09 09:05:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-box(pos = vec(50,0,-25), size = vec(100,100,0), texture = 'https://i.imgur.com/u3haxbY.jpeg')
+b = box(pos = vec(50,0,-25), size = vec(100,100,0), texture = 'https://i.imgur.com/u3haxbY.jpeg')
 
 for q in range(50) :
     box(pos = vec(50,-50,q), size = vec(100,5,50), color = vec(214/255,217/255,216/255), shininess = 0)
@@ -10,4 +10,15 @@ for q in range(50) :
     box(pos = vec(50,-40,q), size = vec(55,10,5), color = vec(60/255,179/255,113/255), shininess = 0)
     box(pos = vec(50,-23,q), size = vec(55,25,5), color = vec(255/255,250/255,240/255), shininess = 0)
     box(pos = vec(50,-35,53), size = vec(10,20,1), color = vec(254/255,245/255,230/255), shininess = 0)
-    cylinder(pos = vec(50,30,50), size = vec(50,50,10), color = vec(0/255,0/255,0/255), shininess = 0)
+    
+while True :
+    rate(50)
+    k = keysdown()
+    if ' ' in k :
+        b.texture = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQruH_sEODrsDkBrnaLGn8Lysog30Uu_BglWWAcJpcqoddPS_mpOcDt_Qc&s=10'
+
+while True :
+    rate(50)
+    w = k()
+    if 'w' in k :
+        b.opacity = random.random()
